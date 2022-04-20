5aca495fd498090aae309ae8598f5ad299303b66
SadiQ HabiL <kingqabil@live.com>
2022-04-21 02:53:55 +0300

    Files added for authorization rules

--- db/migrate/20220414094639_create_posts.rb
+++ db/migrate/20220414094639_create_posts.rb
@@ -3,12 +3,11 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :comments_counter
-      t.integer :likes_counter
-	  t.bigint :author_id
+      t.integer :comments_counter, default: 0, null: false
+      t.integer :likes_counter, default: 0, null: false
+	    t.bigint :author_id
 
       t.timestamps
     end
   end
 end
-
