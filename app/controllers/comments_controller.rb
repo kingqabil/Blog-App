3b6f2d885f6b5f642565bd18d004fc405c4e18b7
atillatahak <atillatahaa@gmail.com>
2022-04-21 17:24:36 +0300

    added authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
-
   def create
     @post = Post.find(params[:post_id])
     new_comment = @post.comments.new(
@@ -16,16 +15,16 @@ class CommentsController < ApplicationController
       render :new, alert: 'Error occured!'
     end
   end
-
   def destroy
-    @comment = Comment.find(params[:commnet_id])
+    @comment = Comment.find(params[:comment_id])
+    @post = Post.find(@comment.post_id)
+    @post.comments_counter -= 1
     @comment.destroy!
+    @post.save
     flash[:success] = 'You have deleted this post!'
     redirect_to user_path(current_user.id)
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:text)[:text]
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,35 +3,55 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
-
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.includes(comments: [:user]).find(params[:id])
   end
-
   def new
     @post = Post.new
   end
-
-  def create
-    new_post = current_user.posts.new(post_params)
-
-    if new_post.save
-      redirect_to user_posts_path(new_post.user.id, new_post.id)
-    else
-      render :new, alert: 'Error occured!'
+  class PostsController < ApplicationController
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts.includes(:comments)
+    end
+    def show
+      @user = User.find(params[:user_id])
+      @post = @user.posts.includes(comments: [:user]).find(params[:id])
+    end
+    def new
+      @post = Post.new
+    end
+    def create
+      new_post = current_user.posts.new(post_params)
+      new_post.likes_counter = 0
+      new_post.comments_counter = 0
+      new_post.update_posts_counter
+      respond_to do |format|
+        format.html do
+          if new_post.save
+            redirect_to "/users/#{new_post.user.id}/posts/", notice: 'Success!'
+          else
+            render :new, alert: 'Error occured!'
+          end
+        end
+      end
+    end
+    def destroy
+      post = Post.find(params[:id])
+      user = User.find(post.user_id)
+      user.posts_counter -= 1
+      post.destroy
+      user.save
+      flash[:success] = 'You have deleted this post!'
+      redirect_to user_path(current_user.id)
+    end
+    private
+    def post_params
+      params.require(:post).permit(:title, :text)
     end
   end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy!
-    flash[:success] = 'You have deleted this post!'
-    redirect_to user_path(current_user.id)
-  end
-
   private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
