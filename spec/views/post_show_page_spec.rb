c6c3ee5360d2f0c833f2d7a7a97a8353dd41defd
SadiQ HabiL <kingqabil@live.com>
2022-04-22 00:49:17 +0300

    Files updated for tests

--- spec/views/post_show_page_spec.rb
+++ spec/views/post_show_page_spec.rb
@@ -1,49 +1,70 @@
-=begin
 require 'rails_helper'
 RSpec.describe 'Login', type: :feature do
   describe 'User' do
     before(:each) do
-      @user1 = User.create! name: 'Sadiq Habil', password: '215234', bio: 'I am Software Engineer',
-                            email: 'kingqabil@gmail.com', confirmed_at: Time.now
-      @user2 = User.create! name: 'Stev', password: '123456', bio: 'My name is Stev',
-                            email: 'stev@gmail.com', confirmed_at: Time.now
+      @user1 = User.create( name: 'Sadiq Habil', password: '215234', bio: 'I am Software Engineer',
+                            email: 'kingqabil@gmail.com', confirmed_at: Time.now)
+      @user2 = User.create( name: 'Stev', password: '123456', bio: 'My name is Stev',
+                            email: 'stev@gmail.com', confirmed_at: Time.now)
       visit root_path
       fill_in 'Email', with: 'kingqabil@gmail.com'
       fill_in 'Password', with: '215234'
       click_button 'Log in'
-      @post1 = @user1.posts.create!(title: 'To Be',
-                                    text: 'The big question is: "To be or not to be a Ruby programmer"')
-      @post2 = @user1.posts.create!(title: 'Hello',
-                                    text: 'Why people say HTML is not a programming language..."')
-      @post3 = @user1.posts.create!(title: 'Hey',
-                                    text: 'With the clif hanger seen in the first half of season 4, do you think..."')
-      @comment1 = @post1.comments.create!(text: 'This is the first comment for the first post', user_id: @user1.id)
-      @comment2 = @post1.comments.create!(text: 'This is the second comment', user_id: @user2.id)
-      @comment3 = @post1.comments.create!(text: 'This is the third comment', user_id: @user2.id)
-      @like1 = @post1.likes.create!(user_id: @user2.id)
-      visit user_post_path(@user1, @post1)
-    end
-    it 'shows posts title' do
-      expect(page).to have_content('To Be')
-    end
-    it 'shows the person who wrote the post' do
-      expect(page).to have_content('Sadiq Habil')
-    end
-    it 'shows number of comments' do
-      expect(page).to have_content('Comment 3')
-    end
-    it 'shows number of likes' do
-      expect(page).to have_content('Likes 1')
-    end
-    it 'can see the post\'s body.' do
-      expect(page).to have_content('This is the first comment for the first post')
-    end
-    it 'can see the username of each commentor.' do
-      expect(page).to have_content 'Stev'
-    end
-    it 'can see the comments of each commentor.' do
-      expect(page).to have_content 'This is the second comment'
-    end
-  end
+      @post1 = Post.create(title: 'To Be',
+        text: 'The big question is: "To be or not to be a Ruby programmer"', comments_counter: 0, likes_counter: 0, user: @user1)
+@post2 = Post.create(title: 'Hello',
+        text: 'Why people say HTML is not a programming language..."', comments_counter: 0, likes_counter: 0, user: @user1)
+@post3 = Post.create(title: 'Hey',
+        text: 'With the clif hanger seen in the first half of season 4, do you think..."', comments_counter: 0, likes_counter: 0, user: @user1)
+
+@comment1 = Comment.create(text: 'This is the first comment for the first post', user: User.first , post: Post.first)
+@comment2 = Comment.create(text: 'This is the second comment', user: User.first , post: Post.first)
+@comment3 = Comment.create(text: 'This is the third comment', user: User.first , post: Post.first)
+
+visit user_post_path(@user1, @post1)
 end
-=end
+
+
+
+
+
+
+
+
+@@ -37,24 +35,30 @@
+
+it 'shows posts title' do
+expect(page).to have_content('To Be')
+end
+it 'shows the person who wrote the post' do
+expect(page).to have_content('Sja')
+end
+
+it 'shows number of comments' do
+user = User.first
+post = Post.first
+expect(page).to have_content(post.comments_counter)
+end
+
+it 'shows number of likes' do
+user = User.first
+post = Post.first
+expect(page).to have_content(post.likes_counter)
+end
+
+it 'can see the post\'s body.' do
+expect(page).to have_content('This is the first comment for the first post')
+end
+
+it 'can see the username of each commentor.' do
+user = User.first
+post = Post.first
+comment = post.comments.first
+expect(page).to have_content(comment.user.name)
+end
+
+it 'can see the comments of each commentor.' do
+expect(page).to have_content 'This is the second comment'
+end
+end
+end
