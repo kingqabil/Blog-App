899fbb06b3eb52527e44ac61cd37058870cad19c
SadiQ HabiL <kingqabil@live.com>
2022-04-23 03:44:12 +0300

    Reviewer Request Change

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,8 @@
-module Api
-  module V1
-    class UsersController < ApplicationController
-      def login
-        valid = User.find_by(email: params[:email]).valid_password?(params[:password])
-        if valid
-          @user = User.find_by(email: params[:email])
-          @user.api_token = Devise.friendly_token.to_s
-          @user.save
-          respond_to do |format|
-            format.json { render json: @user.api_token, status: :ok }
-          end
-        else
-          respond_to do |format|
-            format.json { render json: 'Wrong email or password'.to_json, status: :ok }
-          end
-        end
-      end
-    end
+class Api::V1::UsersController < ApplicationController
+  # before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
   end
 end
